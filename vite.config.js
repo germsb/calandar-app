326bced1ea650ebd086995adc32b045d19866876
germsb <germsb@gmail.com>
2020-08-01 20:29:55 +0200

    add https

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+
 const fs = require("fs");
 module.exports = {
   https: true,
-  // httpsOptions: {
-  //     key: fs.readFileSync("./cert/localhost+1-key.pem"),
-  //     cert: fs.readFileSync("./cert/localhost+1.pem")
-  // }
+  httpsOptions: {
+      key: fs.readFileSync("./cert/localhost+3-key.pem"),
+      cert: fs.readFileSync("./cert/localhost+3.pem")
+  }
 }; //as UserConfig | BuildConfig | ServerConfig | SharedConfig;
